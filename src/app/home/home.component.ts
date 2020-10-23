f3d2fd61fa19cb10e064d412dc968336d61d2e42
tonicprism <gugamatias7@gmail.com>
2020-10-23 15:33:25 -0300

    Using HttpClient in Angular to list problems | adding error handler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from '../data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public title: string = 'Home';
+  products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() {}
+  constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.dataService
+      .sendGetRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any[]) => {
+        console.log(data);
+        this.products = data;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
 }
